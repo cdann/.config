b58e51c322244d8688283714484db0b628e5082a
Celine DANNAPPE <cdannapp@e2r2p6.42.fr>
2015-12-10 15:15:04 +0100

    migration

--- initsubl.sh
+++ initsubl.sh
@@ -1,4 +1,5 @@
-ln -is ~/configs/mycolors ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
-ln -is ~/configs/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-ln -is ~/configs/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
-#cat ~/configs/readme.sublimetext
+ln -is $HOME/conf/mycolors ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+ln -is $HOME/conf/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+ln -is $HOME/conf/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+cat $HOME/conf/readme.sublimetext
+#echo ${0%/*}
